0826432d549993a2c065d864def4ed6c3d72d53d
Carmen chan <carmen.chan@gtomato.com>
2020-05-07 19:58:16 +0800

    ADD jooq library

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.2.6.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("nu.studer.jooq") version "3.0.1"
     kotlin("jvm") version "1.3.71"
     kotlin("plugin.spring") version "1.3.71"
 }
@@ -20,7 +21,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.expediagroup:graphql-kotlin-spring-server:2.1.1")
     implementation("com.expediagroup:graphql-kotlin-schema-generator:2.1.1")
-
+    implementation("org.jooq:jooq:3.11.2")
+    implementation("org.jooq:jooq-meta:3.11.2")
+    implementation("org.jooq:jooq-codegen:3.11.2")
+    implementation("io.reactiverse:reactive-pg-client:0.9.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
