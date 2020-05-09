170d087dcf2ab3b1de0a72c9cbc034d940d96dd1
Carmen chan <carmen.chan@gtomato.com>
2020-05-09 18:31:14 +0800

    ADD entity model convention ADD list query

--- src/main/kotlin/com/gt/spring_graphql/Entity.kt
+++ src/main/kotlin/com/gt/spring_graphql/Entity.kt
@@ -1,28 +1,53 @@
 package com.gt.spring_graphql
 
 import com.gt.spring_graphql.graphql.BookModel
+import com.gt.spring_graphql.graphql.CommentModel
 import com.gt.spring_graphql.graphql.UserModel
 import java.util.*
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "book")
 class BookEntity(
         var name: String?,
         var publishDate: Date?,
-//        var user: UserEntity? = null,
         @Id
         var id: UUID = UUID.randomUUID()
 
 ) {
     constructor() : this("", Date())
     constructor(model: BookModel) : this(model.name, Date())
+
+    @ManyToOne(cascade= [CascadeType.ALL])
+    @JoinColumn(name = "user_id")
+    var user: UserEntity? = null
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "book_comment",
+            joinColumns = [JoinColumn(name = "book_id")],
+            inverseJoinColumns = [JoinColumn(name = "comment_id")])
+    var comments: List<CommentEntity> = emptyList()
 }
 
 @Entity
-@Table(name = "user")
+@Table(name = "comment")
+class CommentEntity(
+        var content: String?,
+        @Id
+        var id: UUID = UUID.randomUUID()
+
+) {
+    constructor() : this("")
+    constructor(model: CommentModel) : this(model.content)
+
+    @ManyToOne(cascade= [CascadeType.ALL])
+    @JoinColumn(name = "user_id")
+    var user: UserEntity? = null
+}
+
+@Entity
+@Table(name = "author")
 class UserEntity(
         var name: String?,
         var avatar: String?,
--- src/main/kotlin/com/gt/spring_graphql/graphql/Model.kt
+++ src/main/kotlin/com/gt/spring_graphql/graphql/Model.kt
@@ -1,14 +1,17 @@
 package com.gt.spring_graphql.graphql
 
 import com.gt.spring_graphql.BookEntity
+import com.gt.spring_graphql.CommentEntity
+import com.gt.spring_graphql.UserEntity
 import org.joda.time.DateTime
+import java.util.*
 
 //@Component
 data class BookModel (
     var id: String,
     var name: String?,
     var publishDate: String,
-    var comments: List<CommentModel>,
+    var comments: List<CommentModel> = emptyList(),
     var author: UserModel?
 ){
     constructor(entity: BookEntity):
@@ -16,20 +19,31 @@ data class BookModel (
                     id = entity.id.toString(),
                     name = entity.name,
                     publishDate = DateTime(entity.publishDate).toString("yyyy/MM/dd"),
-                    comments = emptyList(),
-                    author = null
+                    comments = entity.comments.map{CommentModel(it)},
+                    author = entity.user?.let{ UserModel(it) }
             )
 }
 
 data class CommentModel (
     var id: String,
     var content: String?,
-    var publishDate: String,
-    var user: UserModel
-)
+    var author: UserModel?
+){
+    constructor(entity: CommentEntity): this(
+            id = entity.id.toString(),
+            content = entity.content,
+            author = entity.user?.let{ UserModel(it) }
+    )
+}
 
 data class UserModel (
     var id: String,
     var name: String?,
     var avatar: String?
-)
+){
+    constructor(entity: UserEntity): this(
+            id = entity.id.toString(),
+            name = entity.name,
+            avatar = entity.avatar
+    )
+}
