93bcf92d9cc499f692449d03604154c7485f0615
Carmen chan <carmen.chan@gtomato.com>
2020-05-11 17:08:48 +0800

    ADD book list search and sort

--- src/main/kotlin/com/gt/spring_graphql/Repo.kt
+++ src/main/kotlin/com/gt/spring_graphql/Repo.kt
@@ -1,10 +1,13 @@
 package com.gt.spring_graphql
 
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.domain.Specification
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-interface BookRepo : CrudRepository<BookEntity, UUID> {
-//    override fun findById(id: UUID): Optional<BookEntity>
+interface BookRepository : JpaRepository<BookEntity, UUID> {
+    fun findAll(spec: Specification<*>, pageable: Pageable): Page<BookEntity>
 }
--- src/main/kotlin/com/gt/spring_graphql/graphql/Query.kt
+++ src/main/kotlin/com/gt/spring_graphql/graphql/Query.kt
@@ -1,23 +1,29 @@
 package com.gt.spring_graphql.graphql
 
+import com.expediagroup.graphql.annotations.GraphQLID
 import com.expediagroup.graphql.spring.operations.Query
-import com.gt.spring_graphql.BookRepo
+import com.gt.spring_graphql.BookRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.util.*
 
-@Component
-class BookQuery : Query {
-    @Autowired
-    lateinit var bookRepo: BookRepo
 
-    fun getBookById(id: String): BookModel? {
-        return bookRepo.findById(UUID.fromString(id)).orElse(null)?.let {
+interface BookQuery: Query {
+    fun getBookById(id: String): BookModel?
+    fun list(payload: BookListPayload): BookListModel?
+}
+
+@Component
+class BookQueryImpl : BookQuery {
+    @Autowired
+    private lateinit var bookRepository: BookRepository
+
+    override fun getBookById(@GraphQLID id: String): BookModel? {
+        return bookRepository.findById(UUID.fromString(id)).orElse(null)?.let {
             BookModel(it)
         }
     }
 
-    fun list(): List<BookModel>? = bookRepo.findAll().map{
-        BookModel(it)
-    }
+    override fun list(payload: BookListPayload): BookListModel? = bookRepository.findAll(
+            payload.toSpec(), payload.toPage()).let { BookListModel(it) }
 }
