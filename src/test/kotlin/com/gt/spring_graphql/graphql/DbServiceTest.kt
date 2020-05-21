b145ea80ed985aeee29cca11d2714e50de1dc73e
Carmen chan <carmen.chan@gtomato.com>
2020-05-21 12:01:31 +0800

    ADD jooq seek test

--- src/test/kotlin/com/gt/spring_graphql/graphql/DbServiceTest.kt
+++ src/test/kotlin/com/gt/spring_graphql/graphql/DbServiceTest.kt
@@ -1,10 +1,23 @@
 package com.gt.spring_graphql.graphql
 
+import org.jooq.DSLContext
+import org.jooq.generated.Tables
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class DbServiceTest {
+
+    @Autowired
+    private lateinit var dsl: DSLContext
+
+    @Test
+    fun seek() {
+        dsl.selectFrom(Tables.BOOK)
+                .where()
+                .orderBy(Tables.BOOK.NAME.asc()).seek("book 1").limit(2).fetch()
+    }
 
     @Test
     fun getBookList() {
